2c3a9658a0e4a4e9651a8f8d8f7d3f6b5e90dc65
丁宥寧 <rubyding0525@gmail.com>
2025-11-15 23:34:32 +0800

    Add homepage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="head">
+    <h1>MUGU - 你的學分精算師</h1>
+    <img :src="require('@/assets/Mugu_happy.png')" alt="Mugu" id="mugu-img">
+    <div>
+        <button @click="">註冊</button>
+        <button @click="">登入</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+#head{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+h1 {
+  color: #310d00;
+}
+#mugu-img{
+    width: 30%;
+    height: auto;
+}
+</style>
