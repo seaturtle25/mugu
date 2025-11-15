3dd9bbad3ac4f8bcda30db1bffe64a45e43d6465
unknown <lilyou0721@gmail.com>
2025-11-15 14:29:19 +0800

    Add initial mugu page

---
+++ src/views/mugu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mugu-page">
+    <h1>MUGU</h1>
+    <!-- 之後這裡會放你的表情變化 UI -->
+  </div>
+</template>
+
+<script setup>
+    // 未來在這裡放程式邏輯
+</script>
+
+<style scoped>
+    .mugu-page {
+    padding: 20px;
+    }
+</style>
